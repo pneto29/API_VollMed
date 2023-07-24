e5479f137139bcb082f8f621896aa576e99c402b
Polycarpo Neto <69532484+pneto29@users.noreply.github.com>
2023-07-24 12:27:25 -0300

    Add files via upload

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,37 @@
+package med.voll.api.medico;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+import jakarta.persistence.*;
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Medico(DadosMedicos dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
+}
