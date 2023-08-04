da68930648ad3e73c365df65736375958294664c
Polycarpo Neto <69532484+pneto29@users.noreply.github.com>
2023-08-04 07:21:41 -0300

    Add files via upload

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.DadosMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -24,4 +27,8 @@ public class MedicoController {
 
     }
 
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault (size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
 }
--- api/src/main/java/med/voll/api/medico/DadosMedicos.java
+++ api/src/main/java/med/voll/api/medico/DadosMedicos.java
@@ -16,6 +16,9 @@ public record DadosMedicos(
         @NotBlank
         String email,
         @NotBlank
+        String telefone,
+
+        @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
 
